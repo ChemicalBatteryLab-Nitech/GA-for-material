11ed3727ccb75ea59af14e4ca6ef31b2daed135b
NakayamaLab-NITech <102635361+NakayamaLab-NITech@users.noreply.github.com>
2024-08-12 08:52:08 +0900

    Beta-version
    Faster GA convergence is expected by participating individuals selected by Machine Learning screening.

---
+++ beta-version/inp_ga 1.py
@@ -0,0 +1,37 @@
+"""
+for GA ML
+"""
+mlga = True #T:use ML, F:not use ML
+mlopt1 = 6 #unicross the number of from gstring: ML = populations*ratio - mlopt
+mlopt2 = 6 #twocross
+mlopt3 = 3 #mutation
+mlext1 = 200 #unicross ML candidate
+mlext2 = 200 #twocross ML candidate
+mlext3 = 200 #mutation ML candidate
+
+outMLpred = "sort_label.out" #appear in gstring
+outMLreg = "test_rmse.out"
+
+dirML = "MLrun"
+MLoriginal = "SpecificML"
+
+"""
+for Genetic Algolithm
+
+"""
+POPULATION = 36 #population par generation
+NUM_OF_STRINGS = 1 #num of genes
+MAX_GENERATION = 700 #number of generations
+SAVE = 3 #Number of the most stable genes that are unconditionally transmitted to the next generation
+SURVIVAL_RATE = 0.6 #Percentage of individuals allowed to survive each generation
+CR_2PT_RATE = 0.4 #Percentage of 2-point crossing
+CR_UNI_RATE = 0.4 #Percentage of uniform crossing
+CR_UNI_PB = 0.5 #Probability of occurrence of uniform crossover
+MUTATION_PB = 0.02 #Probability of occurrence of mutation
+STOP_CRITERIA = 900 #Stop condition when best does not change continuously
+RESTART = True #if you want to restart =True
+ELEMENT_FIX = True #if you want to fix the num of elements =True
+
+temp_gene = "temp_gene" #name of the temp gene file
+eval_file = "energy"
+ncore = 18

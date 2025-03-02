09fec1a4796b4e600153bb71ff1a6c2d6990e3ed
Masanobu NAKAYAMA <102635361+Masanobu13@users.noreply.github.com>
2025-03-02 21:36:46 +0900

    Add files via upload

--- inp_ga.py
+++ inp_ga.py
@@ -21,15 +21,15 @@ for Genetic Algolithm
 """
 POPULATION = 24 #population par generation
 NUM_OF_STRINGS = 1 #num of genes
-MAX_GENERATION = 700 #number of generations
+MAX_GENERATION = 500 #number of generations
 SAVE = 3 #Number of the most stable genes that are unconditionally transmitted to the next generation
 SURVIVAL_RATE = 0.6 #Percentage of individuals allowed to survive each generation
 CR_2PT_RATE = 0.4 #Percentage of 2-point crossing
 CR_UNI_RATE = 0.4 #Percentage of uniform crossing
 CR_UNI_PB = 0.5 #Probability of occurrence of uniform crossover
 MUTATION_PB = 0.02 #Probability of occurrence of mutation
-STOP_CRITERIA = 900 #Stop condition when best does not change continuously
-RESTART = True #if you want to restart =True
+STOP_CRITERIA = 100 #Stop condition when best does not change continuously
+RESTART = False #if you want to restart =True
 ELEMENT_FIX = True #if you want to fix the num of elements =True
 
 select_mode = "ranking" #tournament, roulet, ranking
